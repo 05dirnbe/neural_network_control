caf56821a4dd37c08456a13cf0f3ecb9c869489c
mtd <mtd@mpi-inf.mpg.de>
2017-04-18 07:21:39 +0200

    Added logging and zmq sockets to commander. Improved command line argument parsing.

--- commander.py
+++ commander.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python
-import argparse
+import argparse, logging
+import zmq
 
-parser = argparse.ArgumentParser(description='Generates and sends commands to the controller which interacts with the neural network.')
-parser.add_argument('-q', '--quit', metavar="", help='Shutdown controller', type=str, default = "quit" )
-parser.add_argument('-p','--pause', metavar="", help='Put controller in idle state', type=str, default = "pause")
-parser.add_argument('-rw','--read_weights', metavar="", help='Tell controller to read weights', type=str, default = "read_weights")
-parser.add_argument('-ww','--write_weights', metavar="", help='Tell controller to write weights', type=str, default = "write_weights")
-parser.add_argument('-rp','--read_parameters', metavar="", help='Tell controller to read parameters', type=str, default = "read_parameters")
-parser.add_argument('-wp','--write_parameters', metavar="", help='Tell controller to write parameters', type=str, default = "write_parameters")
+port = "5555"
+host = "tcp://localhost:%s" % port
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(name)s: -- %(levelname)s -- %(message)s')
+
+parser = argparse.ArgumentParser(prog="commander", description='Generates and sends commands to the controller which interacts with the neural network.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q', '--quit', help='Shutdown controller', action='store_true')
+group.add_argument('-p','--pause', help='Put controller in idle state', action='store_true')
+group.add_argument('-rw','--read_weights', help='Tell controller to read weights', action='store_true')
+group.add_argument('-ww','--write_weights', help='Tell controller to write weights', action='store_true')
+group.add_argument('-rp','--read_parameters', help='Tell controller to read parameters', action='store_true')
+group.add_argument('-wp','--write_parameters', help='Tell controller to write parameters', action='store_true')
 
 args = parser.parse_args()
+command = None
 
+for arg in vars(args):
+	if getattr(args, arg):
+		command = arg
+
+if command:
+
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	socket.connect(host)
+
+	logging.info("Sending command: %s to port %s at host %s.", command, port, host)
+
+	socket.send(command)
+	message = socket.recv()
+	logging.info("Received reply from server:%s", message)
+
+else:
+	logging.info("No command selected. Use -h | --help to see options.")
