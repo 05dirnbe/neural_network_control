8b7d67dde56abd47701a68671214db7e36559fbe
mtd <mtd@mpi-inf.mpg.de>
2017-04-21 09:10:23 +0200

    Added a program that generates fake camera events via PUB/SUB sockets.

---
+++ shell/communication/python/fake_camera_data.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import argparse, logging, sys, time
+import zmq
+from communication import *
+
+def main(args, context):
+	
+	logger = logging.getLogger(__name__)
+
+	input_data = connect_socket(context, socket_type = zmq.PUB, connection = connections["input_data"])
+
+	logger.info("Starting fake data stream. Sending %d elements.", args.n)
+
+	try:
+
+		for i in xrange(args.n):
+			
+			message = str(i)
+
+			logger.info("Sending: %s", message)
+			input_data.send("Fake data packet %s" % message)
+			time.sleep(args.s)
+		
+		logger.info("Fake data transfer complete.")
+
+	except Exception as e:
+			logger.exception("Failed sending camera data.")
+			logger.info("Shutting down camera data stream.")
+			sys.exit()
+
+	return
+
+if __name__ == '__main__':
+
+	# Setup for application logging
+	logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(name)s: -- %(levelname)s -- %(message)s', filename="./log/camera_data.log", filemode="w")
+	console = logging.StreamHandler()
+	console.setLevel(logging.INFO)
+	formatter = logging.Formatter('%(name)s: -- %(levelname)s -- %(message)s')
+	console.setFormatter(formatter)
+	logging.getLogger("").addHandler(console)
+
+	# Setup for parsing command line arguments
+	parser = argparse.ArgumentParser(prog="fake_camera_data", description='Generates fake camera data.')
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-n', help='Number of data events to be send.', type=int, default=10)
+	group.add_argument('-s', help='Number of seconds to sleep in between two sends.', type=float, default=1)
+	args = parser.parse_args()
+
+	context = zmq.Context()
+
+	main(args, context)
