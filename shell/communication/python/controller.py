68538be17e92eb0eb13e81e32f7fd8e742d029b8
mtd <mtd@mpi-inf.mpg.de>
2017-04-24 15:30:12 +0900

    Major refactoring and improvements of the controller code.

--- shell/communication/python/controller.py
+++ shell/communication/python/controller.py
@@ -1,99 +1,169 @@
 import zmq
 import logging
-import time
-from communication import *
+import time, sys
+import communication
 
-def main(context):
+class Controller(object):
 
-	logger = logging.getLogger(__name__)
+	def __init__(self, connections=communication.connections, topics=communication.topics):
 
-	logger.info("Connecting sockets ...")
+		self.context = zmq.Context()
+		self.connections = connections
+		self.read_commands = ["read_weights", "read_parameters", "read_spikes", "read_topology"]
+		self.write_commands = ["write_weights", "write_parameters", "write_topology"]
 
-	commander = bind_socket(context, socket_type = zmq.REP, connection = connections["controller"])
-	
-	input_data = bind_socket(context, socket_type = zmq.SUB, connection = connections["camera"])
-	input_data.setsockopt(zmq.SUBSCRIBE,"")
-  
-	output_data = bind_socket(context, socket_type = zmq.PUB, connection = connections["monitor"])
+		self.topics = topics
+		self.logger = logging.getLogger("controller")
 
-	logger.info("Initializing poll sets ...")
-	# Initialize poll set
-	poller = zmq.Poller()
-	poller.register(commander, zmq.POLLIN)
-	poller.register(input_data, zmq.POLLIN)
+		self.logger.info("Initializing controller ...")
+		self.logger.info("Connecting sockets ...")
 
-	# Process messages from both sockets
-	
-	logger.info("Controller ready. Listening for commands ...")
-
-	command = None
-
-	while True:
+		self.commander = communication.bind_socket(self.context, socket_type = zmq.REP, connection = self.connections["controller"])
+		self.input_data = communication.bind_socket(self.context, socket_type = zmq.SUB, connection = self.connections["camera"])
+		self.output_data = communication.bind_socket(self.context, socket_type = zmq.PUB, connection = self.connections["monitor"])
 		
-		try:
-			time.sleep(0.5)
-			socks = dict(poller.poll(0))
-		except KeyboardInterrupt:
-			break
+		#input subscribes to any topic, i.e. this socket reads from all sources of input data at once
+		self.input_data.setsockopt(zmq.SUBSCRIBE,"")
 
-		if commander in socks:
-			command = commander.recv()
-			logger.info("Recieved command: %s", command)
-			commander.send(command)
-			logger.debug("Acknowledged command: %s", command)
+		self.logger.info("Initializing poll sets ...")
+		# Initialize poll set
+		self.poller = zmq.Poller()
+		self.poller.register(self.commander, zmq.POLLIN)
+		self.poller.register(self.input_data, zmq.POLLIN)
+		self.logger.info("Initialization complete.")
 
-		if command == "quit":
-			logger.info("Shutting down controller.")
+		self.command = None
+
+
+	def serve_forever(self):
+
+		self.logger.info("Controller ready. Listening for commands ...")
+		
+		while True:
+		
+			try:
+				time.sleep(0.5)
+				socks = dict(self.poller.poll(0))
+			except KeyboardInterrupt:
+				break
+
+			if self.commander in socks:		
+				self.command = self.read_command(self.commander)
+
+			if self.input_data in socks:	
+				camera_data = self.read_camera_data(self.input_data)
+
+			self.handle_command(self.command)
+				
+	def read_command(self, commander):
+		
+		command = commander.recv()
+		self.logger.info("Recieved command: %s", command)
+		commander.send(command)
+		self.logger.debug("Acknowledged command: %s", command)
+
+		return command
+	
+	def handle_command(self, command):
+
+		def remove_prefix(text, prefix):
+			if text.startswith(prefix):
+				return text[len(prefix):]
+			return text
+
+		# handle easy cases immediately
+		if command == None:		return
+		if command == "quit":	self.quit()
+		if command == "pause": 	self.pause()
+
+		if command in self.read_commands:
+
+			topic = remove_prefix(command,"read_")
+			data = self.read_fpga_data(topic=topic)
+			self.publish_fpga_data(self.output_data, data, topic=topic)
+
 			return
 
-		if command == "pause":
-			logger.info("Pausing operation. Controller waiting for commands")
-			command = None
+		if command in self.write_commands:
 
-		if input_data in socks:
-			data = input_data.recv()
-			logger.info("Recieved data: %s", data)
-	   
-		if command == "read_spikes":
-			logger.info("Reading FPGA data: Spikes")
-			time.sleep(1)
-			output_data.send("%s %s" % (topics["spikes"], "Spikes data"))
-			logger.info("Sending monitor data: Spikes")
+			topic = remove_prefix(command,"write_")
+			data = self.read_data(self.commander, topic = topic)
+			self.write_fpga_data(data, topic=topic)
 
-		if command == "read_weights":
-			logger.info("Reading FPGA data: Weights")
-			time.sleep(1)
-			output_data.send("%s %s" % (topics["weights"], "Weights data"))
-			logger.info("Sending monitor data: Weights")
+			return
 
-		if command == "write_weights":
-			time.sleep(1)
-			logger.info("Writing FPGA data: Weights")
-			command = None
+		return
 
-		if command == "read_parameters":
-			logger.info("Reading FPGA data: Parameters")
-			time.sleep(1)
-			output_data.send("%s %s" % (topics["parameters"], "Parameters data"))
-			logger.info("Sending monitor data: Parameters")
+	def read_fpga_data(self, topic):
+		
+		self.logger.info("Reading FPGA data: %s", topic )
 
-		if command == "write_parameters":
-			time.sleep(1)
-			logger.info("Writing FPGA data: Parameters")
-			command = None
+		# fpga helper
+		# data = self.FPGA.read(topic = self.topics[topic])
 
-		if command == "read_topology":
-			logger.info("Reading FPGA data: Topology")
-			time.sleep(1)
-			output_data.send("%s %s" % (topics["topology"], "Network topology"))
-			logger.info("Sending monitor data: Topology")
+		data =  "data"
+		time.sleep(1)
 
-		if command == "write_topology":
-			time.sleep(1)
-			logger.info("Writing FPGA data: topology")
-			command = None
+		return data
 
-	return 
+	def write_fpga_data(self, data, topic):
+		
+		self.logger.info("Writing FPGA data: %s", topic )
+
+		# fpga helper
+		# self.FPGA.write(data, topic = self.topics[topic])
+
+		time.sleep(1)
+
+		self.pause()
+
+	def publish_fpga_data(self, output_data, data, topic):
+
+		self.logger.info("Publishing %s monitor data: %s", topic, data)
+
+		# serialize stuff
+		# buf = self.serializer.write_buffer(data, topic)
+
+		output_data.send("%s %s" % (self.topics[topic], data))
+		
+	def read_camera_data(self, input_data):
+
+		camera_data = input_data.recv()
+		self.logger.info("Recieved data: %s", camera_data)
+		return camera_data
+
+	def read_data(self, socket, topic):
+
+		# buf = socket.recv()
+		# data = self.serializer.deserialize(buf,topic=topic)
+
+		data = "data"
+
+		self.logger.info("Recieved %s data: %s", topic, data)
+		return data
+		
+	def read_camera_data(self, input_data):
+
+		camera_data = input_data.recv()
+		self.logger.info("Recieved data: %s", camera_data)
+		return camera_data
+
+	def quit(self):
+		self.logger.info("Shutting down controller.")
+		sys.exit()
+
+	def pause(self):
+		self.logger.info("Pausing operation. Controller waiting for new commands ...")
+		self.command = None
+
+def main():
+
+		
+	try:
+		controller = Controller()
+		controller.serve_forever()
+	except Exception as e:
+		logging.exception("Something bad happened in the controller. You might want to check the log in /log/controller.log")
 
 if __name__ == '__main__':
 
@@ -105,7 +175,5 @@ if __name__ == '__main__':
 	console.setFormatter(formatter)
 	logging.getLogger("").addHandler(console)
 
-	# Setup for communication
-	context = zmq.Context()
-
-	main(context)
+	main()
+	
