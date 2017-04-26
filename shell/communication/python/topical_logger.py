88c1e35a54d8b23d7caa0d58bc664bc072ece00c
mtd <mtd@mpi-inf.mpg.de>
2017-04-26 09:02:09 +0200

    Simplified data logging/monitoring  by creating monitor classes.

--- shell/communication/python/topical_logger.py
+++ shell/communication/python/topical_logger.py
@@ -4,63 +4,11 @@ import zmq
 import communication
 import configuration
 import serialization
-
-class Data_logger(object):
-
-	def __init__(self, topic, path, sleep = 0, connection = "output_data" , settings = configuration.Config(), serializer = serialization.Serializer()):
-
-		self.context = zmq.Context()
-		self.settings = settings
-		self.serializer = serializer
-		self.sleep = sleep
-
-		self.data = communication.connect_socket(self.context, socket_type = zmq.SUB, connection = self.settings.connections[connection])
-		self.data.setsockopt(zmq.SUBSCRIBE, self.settings.topics[topic])
-
-		self.logger = logging.getLogger(topic + "_logger")
-
-		self.topic = topic
-		self.folder = os.path.join(path, topic)
-		self.log_file = os.path.join(self.folder, topic+"_store"+".txt")
-
-	def prepare_save(self, folder):
-
-		if not os.path.exists(folder):
-			self.logger.info("Creating directory: %s", folder)
-			os.makedirs(folder)
-
-	def serve_forever(self):
-
-		try:
-
-			self.prepare_save(self.folder)
-
-			with open(self.log_file, "w") as store:
-
-				self.logger.info("Starting to log to file: %s", self.log_file)
-
-				while True:
-					
-					topic, message_buffer = self.data.recv().split()
-					message = self.serializer.read_buffer(message_buffer, topic = topic)
-					store.write(message_buffer+"\n")
-					self.logger.debug("Recieved: %s", message)
-
-					time.sleep(self.sleep)
-								
-		except Exception as e:
-			self.logger.exception("Listening failed.")
-			self.logger.info("Shutting down monitor.")
-			sys.exit()
-
-		except KeyboardInterrupt as e:
-			self.logger.info("User interrupt.")
-			self.logger.info("Shutting down monitor.")
-			sys.exit()
+import monitoring
 
 def main(args):
 	
-	data_logger = Data_logger(topic = args.topic, path = args.path, sleep = args.s)
+	data_logger = monitoring.FileLogger(topic = args.topic, path = args.path, sleep = args.s)
 	data_logger.serve_forever()
 
 if __name__ == '__main__':
@@ -80,7 +28,5 @@ if __name__ == '__main__':
 
 	# Setup for application logging
 	logging.basicConfig(level=logging.INFO, format='%(name)s: -- %(levelname)s -- %(message)s')
-	data_logger = Data_logger(topic = args.topic, path = args.path, sleep = args.s)
-	data_logger.serve_forever()
-
+	
 	main(args)
