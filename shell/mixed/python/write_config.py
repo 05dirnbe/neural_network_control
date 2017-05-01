4a895aa1eb483686396de064492063a79c6fe000
mtd <mtd@mpi-inf.mpg.de>
2017-05-01 09:14:10 +0200

    Made configuration writer idiot proof.

--- shell/mixed/python/write_config.py
+++ shell/mixed/python/write_config.py
@@ -5,29 +5,29 @@ import writing
 # Setup for parsing command line arguments
 # see writing module for options
 parser = argparse.ArgumentParser(prog="config file writer", description='Writes the config files the commander uses.')
-parser.add_argument('-a','--write_all_parameters', action='store_true', help='write all config files for commander' )
-parser.add_argument('-p','--write_parameters', action='store_true', help='write parameter config files for commander' )
-parser.add_argument('-t','--write_topology', action='store_true', help='write topology config files for commander')
-parser.add_argument('-w','--write_weights', action='store_true', help='write weights config files for commander')
+group = parser.add_mutually_exclusive_group()
+parser.add_argument('-n','--folder', metavar="config_folder", default = "config", help= "folder to place config files. Topics will be sorted out automatically." )
+group.add_argument('-w','--write_weights', metavar="filename", nargs='?', const = "example.out", help='write weights config files for commander' )
+group.add_argument('-p','--write_parameters', metavar="filename", nargs='?', const = "example.out", help='write parameter config files for commander',)
+group.add_argument('-t','--write_topology', metavar="filename", nargs='?', const = "example.out", help='write topology config files for commander',)
 
 args = parser.parse_args()
 
-if args.write_all_parameters:
-
-	args.write_parameters = args.write_topology = args.write_weights = args.write_all_parameters
 
 if args.write_parameters:
 	print "Writing parametrs config file."
-	topic = writing.ParametersWriter(config_folder = "config")
-	topic.write(filename="example.out")
+	topic = writing.ParametersWriter(config_folder = args.folder)
+	topic.write(filename=args.write_parameters)
 
-if args.write_topology:
+elif args.write_topology:
 	print "Writing topology config file."
-	topic = writing.TopologyWriter(config_folder = "config")
-	topic.write(filename="example.out")
+	topic = writing.TopologyWriter(config_folder = args.folder)
+	topic.write(filename=args.write_topology)
 
-if args.write_weights:
+elif args.write_weights:
 	print "Writing weights config file."
-	topic = writing.WeightsWriter(config_folder = "config")
-	topic.write(filename="example.out")
+	topic = writing.WeightsWriter(config_folder = args.folder)
+	topic.write(filename=args.write_weights)
 	
+else:
+	print "No command selected. Run write_config.py -h for options."
