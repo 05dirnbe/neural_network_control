5cc6a6a2e328f6343ba2fddf72fd9b8ac6150d07
mtd <mtd@mpi-inf.mpg.de>
2017-05-17 07:58:42 +0200

    Added functionality to serialize/deserialize an Eigen matrix.

--- mixed/serialization.hpp
+++ mixed/serialization.hpp
@@ -3,26 +3,84 @@
 
 #include <string>
 #include <zmq.hpp>
+#include <Eigen/Dense>
 
 #include "configuration.hpp"
+#include "cpp_buffer_specifications_generated.h"
 
 
 namespace serialization {
 
     using namespace std;
+    using namespace Buffer;
+    using namespace Eigen;
 
-    
+    typedef int32_t value_t;
+    typedef uint8_t buffer_t;
+    typedef Matrix< value_t, Dynamic, Dynamic, RowMajor > matrix_t;
+    typedef Matrix< value_t, Dynamic, 1 > vector_t;
+
+
+    template < typename M, typename T >
+    const M generate_matrix(const unsigned int nrow, const unsigned int mcol){
+
+        const MatrixXd m = (MatrixXd::Random(nrow,mcol) + MatrixXd::Ones(nrow,mcol) ) * 5;
+        return m.cast< const T >();
+    }
+
     class Serializer_Operations {
 
         public:
 
             const configuration::matrix_t deserialize_matrix( zmq::message_t & buffer) const {
+                
+                const auto mat_restored = GetIntegerMatrix(buffer.data());
+
+                const auto n_restored = mat_restored -> n();
+                const auto m_restored = mat_restored -> m();
+                const auto data_restored = mat_restored -> data();
+                const auto data_size = data_restored -> Length();
+
+                vector_t flat_data_restored(data_size);
+                
+                for (unsigned int i = 0; i < data_size; ++i)
+                    flat_data_restored[i] =  data_restored->Get(i);
+
+                
+                // cout << "restored n to: " << n << endl;
+                // cout << "restored m to: " << m << endl;
+                // cout << "restored data has size: " << data_size << endl;
+
+                // cout << "data restored to : \n" << flat_data_restored << endl;
+                
+                const Map<matrix_t> matrix_restored(flat_data_restored.data(), n_restored, m_restored);
+
+                // cout << matrix_restored << endl;
+
                 return 10;
             }
 
             zmq::message_t serialize_matrix(const configuration::matrix_t data ) const {
-                zmq::message_t empty_buffer;
-                return empty_buffer;
+                
+                auto matrix = generate_matrix<matrix_t, value_t>(2,3);
+
+                const auto n = matrix.rows();
+                const auto m = matrix.cols();
+                const value_t * const flat_data = matrix.data();
+
+                flatbuffers::FlatBufferBuilder builder(0);
+                const auto data_vector = builder.CreateVector(flat_data, n * m );
+                const auto mat = CreateIntegerMatrix(builder, n , m, data_vector);
+                builder.Finish(mat);
+
+                const auto buffer_pointer = builder.GetBufferPointer();
+                const auto buffer_size = builder.GetSize();
+
+                zmq::message_t data_buffer(buffer_size);
+
+                memcpy( data_buffer.data(), buffer_pointer, buffer_size );
+
+                return data_buffer;
             }
 
             const configuration::command_t deserialize_command( zmq::message_t& buffer) const {
